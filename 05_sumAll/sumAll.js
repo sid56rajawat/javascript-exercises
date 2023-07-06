e075a56b016f49a9a38114a3b1ba80ad82411c55
sid56rajawat <sid56rajawat@gmail.com>
2023-07-06 13:25:21 +0530

    add solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
+function isValid(n){
+    return ((typeof n === "number") && n >= 0);
+}
+const sumAll = function(start,end) {
+    if(!(isValid(start) && isValid(end))) return "ERROR";
+    if(start > end) [start,end] = [end,start]; 
+    let sum = 0;
+    for(let i=start;i<=end;i++){
+        sum += i;
+    }
+    return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
