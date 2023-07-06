a4361decdd8393af05da07b8a8f07150b68eefb1
sid56rajawat <sid56rajawat@gmail.com>
2023-07-06 12:33:59 +0530

    add solution to 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string,number) {
+    if(number < 0) return "ERROR";
+    let repeatedString = "";
+    while(number--) repeatedString += string;
+    return repeatedString;
 };
 
 // Do not edit below this line
