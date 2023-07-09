b141e3701b9b126bc1cd85a601594687356300a2
sid56rajawat <sid56rajawat@gmail.com>
2023-07-09 18:41:33 +0530

    Add solution to "12_findTheOldest"

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    // people is an array of object
+    // we can sort this based on their age then return people[0]
+    people.sort((a,b) => {
+        let aAge = (a.yearOfDeath || (new Date).getFullYear()) - a.yearOfBirth;
+        let bAge = (b.yearOfDeath || (new Date).getFullYear()) - b.yearOfBirth;
+        return bAge - aAge;
+    });
+    return people[0];
 };
 
 // Do not edit below this line
