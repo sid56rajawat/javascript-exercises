02b9cdc474c9794bf058c7d18633a1275e5f7e5f
sid56rajawat <sid56rajawat@gmail.com>
2023-07-09 18:11:20 +0530

    Add solution to "10_fibonacci"

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+let memo = [0,1,1];
 
+const fibonacci = function(n) {
+    if(n < 1) return "OOPS";
+    if(memo[n]) return memo[n];
+    memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+    return memo[n];
 };
 
 // Do not edit below this line
